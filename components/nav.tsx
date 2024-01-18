286404ac5e69bdfd42bca2bb8ab616bd5ec81993
Lopl <carlanft1@gmail.com>
2024-01-18 14:40:58 -0300

    navbar

--- components/nav.tsx
+++ components/nav.tsx
@@ -133,18 +133,6 @@ function Nav() {
               className="flex cursor-pointer gap-1 hover:text-brickly500"
             >
               Comunidad
-              <svg
-                className="-mr-1 h-5 w-5 text-gray-400 group-hover:fill-brickly500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
             </div>
 
             {isOpen ? (
