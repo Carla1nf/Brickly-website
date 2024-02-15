21dca3444431b3f4699d238626409a10c4759de9
Lopl <carlanft1@gmail.com>
2024-02-16 00:53:38 +0100

    s

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false, net: false, tls: false }
-      config.externals.push("pino-pretty", "lokijs", "encoding")
-      return config
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  reactStrictMode: true,
+  fallback: false,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
